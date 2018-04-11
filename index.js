415aac0a488c4ec9fe2b441832e2f7a55ade54c5
shalldie <394229785@qq.com>
2018-04-11 15:25:58 +0800

    >_<#@!  first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const nunjucks = require('nunjucks');
+
+/**
+ * nunjucks 中间件
+ * 
+ * @param {any} path 模板根目录路径
+ * @param {string} [ext='.html'] 模板扩展名
+ * @param {any} [config={}] nunjucks 配置
+ * @returns {Function}
+ */
+module.exports = (path, ext = '.html', config = {}) => {
+    const env = nunjucks.configure(path, config);
+    return async (ctx, next) => {
+        ctx.render = async (view, context) => {
+            const data = Object.assign({}, ctx.state, context);
+            return new Promise((resolve, reject) => {
+                env.render(view + ext, data, (err, content) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    ctx.type = 'html';
+                    ctx.body = content;
+                    resolve(content);
+                });
+            });
+        };
+        await next();
+    };
+};
